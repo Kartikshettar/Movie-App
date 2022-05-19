5431f698a579fb3c42983b95859da5d71ff87a78
Kartik <86262379+Kartikshettar@users.noreply.github.com>
2022-05-19 18:31:10 +0530

    Checkpoint 6 part-2

--- server.js
+++ server.js
@@ -1,47 +1,35 @@
-var express = require('express');
-var app = express();
+// var express = require('express');
+// var app = express();
 
-app.get('/movies', function(req, res) {
-    res.send("All Movies Data in JSON format from Mongo DB");
-  });
+// app.get('/movies', function(req, res) {
+//     res.send("All Movies Data in JSON format from Mongo DB");
+//   });
 
-app.get('/genres', function(req, res) {
-    res.send("All Genres Data in JSON format from Mongo DB");
-   });
-// define the about route
-app.get('/artists', function(req, res) {
-    res.send("All Artists Data in JSON format from Mongo DB");
-   });
-   
-   const db = require("./app/models");
-   db.mongoose
-     .connect(db.url, {
-       useNewUrlParser: true,
-       useUnifiedTopology: true
-     })
-     .then(() => {
-       console.log("Connected to the database!");
-       
-     })
-     .catch(err => {
-       console.log("Cannot connect to the database!", err);
-       process.exit();
-     });
-
-     const db = require("./app/models");
-db.mongoose
-  .connect(db.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log("Connected to the database!");
-    
-  })
-  .catch(err => {
-    console.log("Cannot connect to the database!", err);
-    process.exit();
-  });
+// app.get('/genres', function(req, res) {
+//     res.send("All Genres Data in JSON format from Mongo DB");
+//    });
+// // define the about route
+// app.get('/artists', function(req, res) {
+//     res.send("All Artists Data in JSON format from Mongo DB");
+//    }); 
 
 
-app.listen(9000);
+
+const express = require('express');
+const app = express();
+var cors = require('cors');
+
+
+
+const options = {
+  origin: 'http://localhost:3000/api',
+  }
+  app.use(cors(options))
+
+
+ app.get("/", cors(options), function (req, res) {
+  res.json({ message: "Welcome to Upgrad Movie booking application development." });
+})
+
+
+app.listen(3000);
